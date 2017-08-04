7ce1b670d4430d33ef1261a306c6918f993f36ac
nguyenvantra <dkdarknessnvt@gmail.com>
2017-08-04 23:50:57 +0700

    add src arraylist

---
+++ src/com/darkness/listinterface/arraylist/ArrayListTest.java
@@ -0,0 +1,23 @@
+package com.darkness.listinterface.arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+	public static void main(String[] args) {
+		/*
+		 * initial array list
+		 * */
+		//Constructs an empty list
+		ArrayList list1 = new ArrayList();
+		
+		//Constructs an empty list with the specified initial capacity.
+		ArrayList list2 = new ArrayList(10);
+		
+		//Constructs a list containing the elements of the specified collection.
+		ArrayList<Integer> list3 = new ArrayList<>();
+		
+		//
+		
+		
+	}
+}
