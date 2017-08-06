aeea696e063d4b1e1edca5192081e7bb7cc80a18
nguyenvantra <dkdarknessnvt@gmail.com>
2017-08-06 22:29:15 +0700

    update

---
+++ src/com/darkness/listinterface/vector/VectorTest.java
@@ -0,0 +1,13 @@
+package com.darkness.listinterface.vector;
+
+import java.util.Vector;
+
+public class VectorTest {
+	public static void main(String[] args) {
+		Vector<String> list = new Vector<>();
+		
+		int[] arr = null;
+		
+		arr[0] = 1;
+	}
+}
