98feb9fd24a4d7429dfa83b9b3e823e94beb86c3
nguyenvantra <dkdarknessnvt@gmail.com>
2017-08-18 23:03:19 +0700

    update src treeset

---
+++ src/com/darkness/setinterface/treeset/TreeSetTest.java
@@ -0,0 +1,13 @@
+package com.darkness.setinterface.treeset;
+
+import java.util.TreeSet;
+
+public class TreeSetTest {
+	public static void main(String[] args) {
+		TreeSet<String> dummyList = new TreeSet<>();
+		
+		dummyList.add(null);
+		
+		System.out.println(dummyList);
+	}
+}
